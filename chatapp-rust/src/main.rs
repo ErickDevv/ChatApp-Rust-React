c4caf62e9338cd6f0feeda852e14ce42e6526a02
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-07-18 00:57:29 -0600

    Start of reading the request

--- chatapp-rust/src/main.rs
+++ chatapp-rust/src/main.rs
@@ -1,15 +1,28 @@
 use salvo::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[handler]
 async fn hello() -> &'static str {
     "Hello World"
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    username: String,
+    password: String,
+}
+
+#[handler]
+async fn signup(req: &mut Request, res: &mut Response) {
+    let user = req.parse_body::<User>().await;
+    res.status_code(StatusCode::CREATED);
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().init();
 
-    let router = Router::new().get(hello);
+    let router = Router::new().push(Router::new().path("signup").post(signup));
 
     let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
 
