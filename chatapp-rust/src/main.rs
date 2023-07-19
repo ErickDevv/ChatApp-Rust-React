4b508ae4d1f6a6c49a419ad137ece50d38cecf72
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-07-18 18:04:37 -0600

    Add jwt

--- chatapp-rust/src/main.rs
+++ chatapp-rust/src/main.rs
@@ -1,5 +1,16 @@
+use jsonwebtoken::{self, EncodingKey};
 use salvo::prelude::*;
 use serde::{Deserialize, Serialize};
+use time::{Duration, OffsetDateTime};
+
+//TODO: Read from env variable or some file.
+const SECRET_KEY: &str = "";
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JwtClaims {
+    user_id: String,
+    exp: i64,
+}
 
 #[handler]
 async fn hello() -> &'static str {
@@ -12,10 +23,51 @@ struct User {
     password: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Token {
+    token: String,
+}
+
 #[handler]
 async fn signup(req: &mut Request, res: &mut Response) {
-    let user = req.parse_body::<User>().await;
-    res.status_code(StatusCode::CREATED);
+    let token = match req.parse_body::<User>().await {
+        Ok(user) => {
+            let token = generate_token(user.username.as_str());
+            token
+        }
+        Err(_err) => {
+            res.status_code(StatusCode::BAD_REQUEST);
+            return;
+        }
+    };
+
+    let render_token = match token {
+        Ok(token) => Token { token },
+        Err(_err) => {
+            res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
+            return;
+        }
+    };
+
+    res.render(Json(render_token));
+}
+
+fn generate_token(user_id: &str) -> Result<String, String> {
+    let claims = JwtClaims {
+        user_id: user_id.to_owned(),
+        exp: (OffsetDateTime::now_utc() + Duration::days(15)).unix_timestamp(),
+    };
+
+    let token = match jsonwebtoken::encode(
+        &jsonwebtoken::Header::default(),
+        &claims,
+        &EncodingKey::from_secret(SECRET_KEY.as_bytes()),
+    ) {
+        Ok(token) => token,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    Ok(token)
 }
 
 #[tokio::main]
