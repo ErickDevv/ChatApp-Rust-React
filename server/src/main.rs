0fe086e65a9b49c16cba24ba50d56590357bafc9
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2023-07-26 20:33:19 -0600

    Update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -127,7 +127,7 @@ async fn main() {
         .push(Router::new().path("signup").post(signup));
     //.push(Router::new().path("signin").post(signin));
 
-    let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+    let acceptor = TcpListener::new("0.0.0.0:5800").bind().await;
 
     Server::new(acceptor).serve(router).await;
 }
